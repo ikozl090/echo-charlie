75ce6ae8249478c1df68993df048faea60bceff9
Pooja Ravi <poojaravi@Poojas-MacBook-Pro.local>
2025-10-26 10:16:57 -0400

    Adding video order

--- EchoCharlie/streamlit.py
+++ EchoCharlie/streamlit.py
@@ -8,6 +8,8 @@ st.title("CharlieYaplin")
 st.write("Hello from VS Code!")
 
 echo_db = EchoDB(db_path="./demo_db_3", collection_name = "demo_collection_3", audio_db_name = "demo_audio_3.db")
+main_path = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/"
+
 
 st.subheader("Add reference videos to Database:")
 
@@ -30,7 +32,7 @@ to_db = ""
 
 with col1:
     if st.button("Add Trudeau to DB"):
-        to_db = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/videos/trudeau_ref.mp4"
+        to_db = main_path + "videos/trudeau_ref.mp4"
         st.session_state["choice"] = "img1"
         echo_db.push_video(to_db)
         st.video(to_db)
@@ -38,7 +40,7 @@ with col1:
 
 with col2:
     if st.button("Add Obama to DB"):
-        to_db = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/videos/obama_ref.mp4"
+        to_db = main_path + "videos/obama_ref.mp4"
         st.session_state["choice"] = "img2"
         echo_db.push_video(to_db)
         st.video(to_db)
@@ -46,21 +48,18 @@ with col2:
 with col3:
     if st.button("Add Macron to DB"):
         st.session_state["choice"] = "img3"
-        to_db = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/videos/macron_ref.mp4"
+        to_db = main_path + "videos/macron_ref.mp4"
         echo_db.push_video(to_db)
         st.video(to_db)
 
 
 with col4:
     if st.button("Add Trump to DB"):
-        to_db = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/videos/trump_ref.mp4"
+        to_db = main_path + "videos/trump_ref.mp4"
         st.session_state["choice"] = "img4"
         echo_db.push_video(to_db)
         st.video(to_db)
     
-
-
-
 
 #uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "mov", "avi"])
 st.subheader("Choose from the following videos:")
@@ -69,26 +68,24 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
     if st.button("🖼️ Video 1"):
         st.session_state["choice"] = "img1"
-    st.image("/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/st_display/trudeau_3.png", caption="Trudeau")
+    st.image(main_path + "st_display/trudeau_3.png", caption="Trudeau")
 
 with col2:
     if st.button("🖼️ Video 2"):
         st.session_state["choice"] = "img2"
-    st.image("/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/st_display/obama_3.png", caption="Obama")
+    st.image(main_path + "st_display/obama_3.png", caption="Obama")
 
 with col3:
     if st.button("🖼️ Video 3"):
         st.session_state["choice"] = "img3"
-    st.image("/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/st_display/macron_1.png", caption="Macron")
-
+    st.image(main_path + "st_display/macron_1.png", caption="Macron")
 
 with col4:
     if st.button("🖼️ Video 4"):
         st.session_state["choice"] = "img4"
-    st.image("/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/st_display/trump_1.png", caption="Option 3")
+    st.image(main_path + "st_display/trump_1.png", caption="Option 3")
 
 
-# --- Display different responses depending on which image was clicked ---
 choice = st.session_state.get("choice", None)
 path = ""
 if choice == "img1":
@@ -109,7 +106,7 @@ elif choice == "img3":
 elif choice == "img4":
     #st.subheader("You clicked Image 3!")
     #st.write("Response for Image 3: You could run another model or show predictions.")
-    path = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/videos/trump_1.mp4"
+    path = main_path + "videos/trump_1.mp4"
     st.video(path,muted=True)
 
 else:
@@ -118,10 +115,11 @@ else:
 
 if st.button("Generate Audio"):
     get_path = path
+    person = path.split("/")[-1].split(".")[0] + "_generated.wav"
     api_key = "bai-KexSTrjlCNGM_ZhBteIErVDK9saJ8NbbHsRInNm5YTIF4b4L"
-    out = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/audio/output_sample3.wav"
-    transcripts = "/Users/poojaravi/Documents/code/GitHub/echo-charlie/data/transcripts/transcript.json"
+    out = main_path + "audio/output_sample3.wav"
+    transcripts = main_path + "transcripts/transcript.json"
     #echo_charlie = EchoCharlie(video_path=path,transcripts=transcripts,qwen_api_key=api_key,higgs_api_key=api_key)
     #_, aud = echo_charlie.forward(out_path=out)
-    st.audio(aud, format="audio/wav")
+    st.audio(main_path+person, format="audio/wav")
     

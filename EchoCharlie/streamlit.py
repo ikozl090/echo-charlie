7c3822a556cf19d274db957a245440426c92dd04
Pooja Ravi <poojaravi@Poojas-MacBook-Pro.local>
2025-11-27 15:34:19 -0500

    streamlit interface changes

--- EchoCharlie/streamlit.py
+++ EchoCharlie/streamlit.py
@@ -5,10 +5,10 @@ from echo_db import EchoDB
 
 st.title("CharlieYaplin")
 
-st.write("Hello from Itay, Pooja and Vishnou!")
+st.write("Hello from Itay, Pooja and Vishnou! <3")
 
 echo_db = EchoDB(db_path="./demo_db_3", collection_name = "demo_collection_3", audio_db_name = "demo_audio_3.db")
-main_path = "data/"
+main_path = "/data/"
 
 
 st.subheader("Add reference videos to Database:")
@@ -17,13 +17,10 @@ uploaded_file = st.file_uploader("Upload any video file to Database", type=["mp4
 #print(uploaded_file)
 #echo_db.push_video(uploaded_file)
 
-# Step 3: Create a local save directory
 save_dir = "uploads/saved.mp4"
 
-# Step 4: Construct the full local path
 #local_path = os.path.join(save_dir, uploaded_file.name)
 
-# Step 5: Save the uploaded file to disk
 #with open(save_dir, "wb") as f:
     #f.write(uploaded_file.getbuffer())
 
@@ -31,16 +28,16 @@ col1, col2, col3, col4 = st.columns(4)
 to_db = ""
 
 with col1:
-    if st.button("Add Trudeau to DB"):
-        to_db = main_path + "videos/trudeau_ref.mp4"
+    if st.button("Add FeiFei Li to DB"):
+        to_db = main_path + "videos/feifei.mp4"
         st.session_state["choice"] = "img1"
         echo_db.push_video(to_db)
         st.video(to_db)
 
 
 with col2:
-    if st.button("Add Obama to DB"):
-        to_db = main_path + "videos/obama_ref.mp4"
+    if st.button("Add Tom to DB"):
+        to_db = main_path + "videos/tom.mp4"
         st.session_state["choice"] = "img2"
         echo_db.push_video(to_db)
         st.video(to_db)
@@ -68,33 +65,33 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
     if st.button("🖼️ Video 1"):
         st.session_state["choice"] = "img1"
-    st.image(main_path + "st_display/trudeau_3.png", caption="Trudeau")
+    st.image(main_path + "st_display/feifei_1.png", caption="Fei Fei Li")
 
 with col2:
     if st.button("🖼️ Video 2"):
         st.session_state["choice"] = "img2"
-    st.image(main_path + "st_display/obama_3.png", caption="Obama")
+    st.image(main_path + "st_display/tom_1.png", caption="Tom Holland")
 
 with col3:
     if st.button("🖼️ Video 3"):
         st.session_state["choice"] = "img3"
-    st.image(main_path + "st_display/macron_1.png", caption="Macron")
+    st.image(main_path + "st_display/macron_1.png", caption="Emmanuel Macron")
 
 with col4:
     if st.button("🖼️ Video 4"):
         st.session_state["choice"] = "img4"
-    st.image(main_path + "st_display/trump_1.png", caption="Option 3")
+    st.image(main_path + "st_display/trump_1.png", caption="Donald Trump")
 
 
 choice = st.session_state.get("choice", None)
 path = ""
 if choice == "img1":
     #st.write(" video.")
-    path = main_path + "muted_videos/trudeau_3.mp4"
+    path = main_path + "muted_videos/feifei_1.mp4"
     st.video(path,muted=True)
 
 elif choice == "img2":
-    path = main_path + "muted_videos/obama_3_one_word_error.mp4"
+    path = main_path + "muted_videos/tom_1.mp4"
     st.video(path,muted=True)
 
 elif choice == "img3":
@@ -116,16 +113,21 @@ else:
 if st.button("Generate Audio"):
     get_path = path
     person = path.split("/")[-1].split(".")[0] + "_generated.wav"
-    api_key = ""
+    api_key = "bai-KexSTrjlCNGM_ZhBteIErVDK9saJ8NbbHsRInNm5YTIF4b4L"
     out = main_path + "audio/output_sample3.wav"
     transcripts = main_path + "transcripts/transcript.json"
     #echo_charlie = EchoCharlie(video_path=path,transcripts=transcripts,qwen_api_key=api_key,higgs_api_key=api_key)
     #_, aud = echo_charlie.forward(out_path=out)
     st.audio(main_path+"generated_audio/"+person, format="audio/wav")
     
+    
 if st.button("Generate Video"):
     person_vid = path.split("/")[-1].split(".")[0] + "_unmuted.mp4"
     st.video(main_path+"generated_videos/"+person_vid)
+    
+if st.button("Generated vs. Reference Audio"):
+    person_plot = path.split("/")[-1].split(".")[0] + "_plot.png"
+    st.image(main_path+"plots/"+person_plot,caption="Audio Comparison Visualization")
 
 if st.button("Original Video"):
     person_vid = path.split("/")[-1].split(".")[0] + ".mp4"
